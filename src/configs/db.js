2ff4d225289c4ba9fcdfe94780af43c9a2e987ee
vikashmahato23 <94161952+vikashmahato23@users.noreply.github.com>
2022-03-21 10:53:28 +0530

    df
    df

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://vikash1997:Jyotibharti12@cluster0.mhoai.mongodb.net/c3?retryWrites=true&w=majority"
+    "mongodb+srv://vikash1997:Jyotibharti12@cluster0.mhoai.mongodb.net/c2?retryWrites=true&w=majority"
   );
 };
